d3e1c0b7509c97976ef38d6d41685a6967a10271
Chris Holmes <tochrisholmes@gmail.com>
2013-05-26 17:27:14 +0100

    Added a helper module

---
+++ app/helpers/refinery/tweets/tweets_helper.rb
@@ -0,0 +1,9 @@
+module Refinery
+  module Tweets
+    module TweetsHelper
+      def tweets
+        "Here are the tweets"
+      end
+    end
+  end
+end
--- lib/refinery/tweets.rb
+++ lib/refinery/tweets.rb
@@ -20,9 +20,9 @@ module Refinery
         @root ||= Pathname.new(File.expand_path('../../../', __FILE__))
       end
 
-      # def factory_paths
-      #   @factory_paths ||= [ root.join("spec/factories").to_s ]
-      # end
+      def factory_paths
+        @factory_paths ||= [ root.join("spec/factories").to_s ]
+      end
     end
   end
 end
