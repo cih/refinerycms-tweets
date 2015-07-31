af2d79654dbe415d49e8902297a032a71fa8b652
Marshal Linfoot <mlinfoot@gmail.com>
2015-07-31 13:44:20 -0400

    Refactored for Rspec 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,35 +1,3 @@
-require 'rubygems'
-
-# Configure Rails Environment
-ENV["RAILS_ENV"] ||= 'test'
-
-require File.expand_path("../dummy/config/environment", __FILE__)
-
-require 'rspec/rails'
-require 'capybara/rspec'
-
-Rails.backtrace_cleaner.remove_silencers!
-
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.mock_with :rspec
-  config.filter_run :focus => true
-  config.run_all_when_everything_filtered = true
-  config.expose_current_running_example_as :example
-  config.infer_spec_type_from_file_location!
-end
-
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories including factories.
-([Rails.root.to_s] | ::Refinery::Plugins.registered.pathnames).map{|p|
-  Dir[File.join(p, 'spec', 'support', '**', '*.rb').to_s]
-}.flatten.sort.each do |support_file|
-  require support_file
-end
-
-def settings_hash(twitter_account)
-  { "username" => twitter_account.username,
-    "tweet_count" => twitter_account.tweet_count,
-    "visible" => twitter_account.visible,
-    "widget_id" => twitter_account.widget_id
-  }
 end
